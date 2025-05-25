c855ab02cadd5fe57b8fb8cf4944f0a66142f146
Ovidiojcf <ojcfarias@gmail.com>
2025-05-25 19:13:27 -0300

    feat(home): add a data names on project and display in UI

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,7 +9,20 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 // Add a state variable to render in home.component.html
   myBoolean: boolean = false;
-
+  myData: { id: number; name: string }[] = [
+    {
+      id: 1,
+      name: 'Pernalonga'
+    },
+    {
+      id: 2,
+      name: 'Mickey'
+    },
+    {
+      id: 3,
+      name: 'Donald'
+    }
+  ];
   // Add a method to toggle the state variable
   handleBoolean() {
     this.myBoolean = !this.myBoolean;
